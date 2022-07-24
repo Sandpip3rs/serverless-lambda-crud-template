1f34f6888c3ba034fc18d7d776e56d8826094ebe
Matto <muhamadto@gmail.com>
2022-07-24 11:14:41 +1000

    added http error support with automatic JSON parsing

--- src/functions/delete/handler.ts
+++ src/functions/delete/handler.ts
@@ -1,19 +1,19 @@
 import 'source-map-support/register';
 
-import { formatJSONResponse } from '@/libs/api-gateway';
-import { middyfy } from '@/libs/lambda';
+import {formatJSONResponse} from '@/libs/api-gateway';
+import {middyfy} from '@/libs/lambda';
+import createHttpError from 'http-errors';
 
 const remove = async (event) => {
   const idPathVariable = 'id';
   if (event.pathParameters && event.pathParameters[idPathVariable]) {
     const id = event.pathParameters[idPathVariable];
     console.info(`Deleting account '${id}' ...`);
-    return formatJSONResponse({ message: 'All good' });
+    return formatJSONResponse({message: 'All good'});
   }
 
   console.error("'id' is missing");
-  const errorBody = { error: "'id' is missing" };
-  return formatJSONResponse(errorBody, 400);
+  createHttpError(400, "'id' is missing");
 };
 
 export const main = middyfy(remove);
--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,6 +1,13 @@
 import middy from '@middy/core';
 import middyJsonBodyParser from '@middy/http-json-body-parser';
+import httpErrorHandler from '@middy/http-error-handler'
+// import validator from '@middy/validator'
+import JSONErrorHandlerMiddleware from 'middy-middleware-json-error-handler'
 
 export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser());
+  return middy(handler)
+  // use(validator({eventSchema}))
+  .use(middyJsonBodyParser())
+  .use(httpErrorHandler())
+  .use(JSONErrorHandlerMiddleware());
 };
